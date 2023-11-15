c21817bb4f9071efb5483cf996b0ceddce471d75
Ji Yook <azure6.dev@gmail.com>
2023-11-15 17:47:50 +0900

    231115 Linked list - Constructor

---
+++ 03-Linked Lists/01-LL-Constructor.py
@@ -0,0 +1,46 @@
+# class LinkedList:
+    # def __init__(self, value):
+        # create new Node
+    # def append(self, value):
+        # create new Node
+        # add Node to end
+    # def prepend(self, value):
+        # create new Node
+        # add Node to beginning
+    # def insert(self, value):
+        # create new Node
+        # insert Node
+        
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+ 
+my_linked_list = LinkedList(4)
+
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 4
+    Tail: 4
+    Length: 1
+    
+"""
+
+                                                                                                                    
